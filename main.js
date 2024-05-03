ffe0bc37d8ceda673b66393b095f7cd919af300c
itallohiago <110993384+itarohiago@users.noreply.github.com>
2024-05-03 10:18:35 -0300

    Add files via upload

--- main.js
+++ main.js
@@ -1,14 +1,3 @@
-const botões = document.querySelectorAll(".botao");
-
-for(let i=0;i <botoes.length;i++){
-    botoes[i].onclick = function(){
-        
-        for(let j=0;j<botoes.length;j++){
-            botoes[j].classList.remove("ativo");
-        }
-        botoes[i].classList.add("ativo");
-    }
-}
 const botoes = document.querySelectorAll(".botao");
 const textos = document.querySelectorAll(".aba-conteudo");
 
@@ -26,11 +15,13 @@ for (let i = 0; i < botoes.length; i++) {
 }
 
 const contadores = document.querySelectorAll(".contador");
-const tempoObjetivo = new Date("2023-10-05T00:00:00");
-let tempoAtual = new Date();
+const tempoObjetivo1 = new Date("2024-10-05T00:00:00");
+const tempoObjetivo2 = new Date("2024-12-05T00:00:00");
+const tempoObjetivo3 = new Date("2024-12-30T00:00:00");
+const tempoObjetivo4 = new Date("2025-02-01T00:00:00");
 
-contadores[0].textContent = tempoObjetivo1 - tempoAtual;
-contadores[0].textContent = calculaTempo(tempoObjetivo1);
+const tempos = [tempoObjetivo1,tempoObjetivo2,tempoObjetivo3,tempoObjetivo4];
+
 
 function calculaTempo(tempoObjetivo) {
     let tempoAtual = new Date();
@@ -43,41 +34,25 @@ function calculaTempo(tempoObjetivo) {
     segundos %= 60;
     minutos %= 60;
     horas %= 24;
- 
- return dias + " dias " + horas + " horas " + minutos + " minutos " + segundos + " segundos";
-}
-const tempoObjetivio1 = new Date("2023-10-05T00:00:00");
-const tempoObjetivo2 = new Date("2023-12-05T00:00:00");
-const tempoObjetivo3 = new Date("2023-12-30T00:00:00");
-const tempoObjetivo4 = new Date("2024-02-01T00:00:00");
-contadores[0].textContent = calculaTempo(tempoObjetivo1);
-contadores[1].textContent = calculaTempo(tempoObjetivo2);
-const tempos = [tempoObjetivo1, tempoObjetivo2, tempoObjetivo3, tempoObjetivo4];
-contadores[0].textContent = calculaTempo(tempos[0]);
-function atualizaCronometro(){
-    for (let i=0; i<contadores.length;i++){
-        contadores[i].textContent = calculaTempo(tempos[i]);   
+    if (tempoFinal > 0){
+        return [dias,horas,minutos,segundos];
+    } else {
+        return [0,0,0,0];
     }
 }
+
+function atualizaCronometro(){
+    for (let i=0; i<contadores.length;i++){
+        document.getElementById("dias"+i).textContent = calculaTempo(tempos[i])[0];
+        document.getElementById("horas"+i).textContent = calculaTempo(tempos[i])[1];
+        document.getElementById("min"+i).textContent = calculaTempo(tempos[i])[2];
+        document.getElementById("seg"+i).textContent = calculaTempo(tempos[i])[3];   
+    }
+}
+
 function comecaCronometro(){
     atualizaCronometro();
-    setInterval(atualizaCronometro, 1000);
+    setInterval(atualizaCronometro,1000);
 }
 
 comecaCronometro();
-const tempoObjetivo1 = new Date("2020-10-05T00:00:00");
-if (tempoFinal > 0){
-    return dias + " dias " + horas + " horas " + minutos + " minutos " + segundos + " segundos";
-} else {
-    return [0,0,0,0];
-}
-function atualizaCronometro(){
-    document.getElementById("dias0").textContent = calculaTempo(tempos[0])[0];
-    document.getElementById("horas0").textContent = calculaTempo(tempos[0])[1];
-    document.getElementById("min0").textContent = calculaTempo(tempos[0])[2];
-    document.getElementById("seg0").textContent = calculaTempo(tempos[0])[3];
-
-    for (let i=0; i<contadores.length;i++){
-       // contadores[i].textContent = calculaTempo(tempos[i]);   
-    }
-}
